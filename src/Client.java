c5e9975a63d05916a9f446226ad097421882a3f8
bazgrim <joshuakronemeyer@gmail.com>
2016-11-04 01:17:33 -0700

    Added vars and basic server connection setup

--- src/Client.java
+++ src/Client.java
@@ -1,4 +1,27 @@
 
 public class Client {
+	
+	// Declare Vars
+	boolean myTurn = false;
+	boolean wait = true;
+	String host = "127.0.0.1";
+	int port = 8081;
+	
+	public void getServerInfo() {
+		// Enter Server IP
+		// Enter Server Port
+		// Check format of IP & Port
+		
+	}
+	
+	//@param1 Host of server
+	//@param2 Port of server
+	//@return Server connected status
+	public boolean connectToServer(String h, int p) {
+		// Attempt to connect to server
+		// If connection fails, return false
+		// else
+		return true;
+	}
 
 }
--- src/Player.java
+++ src/Player.java
@@ -1,10 +1,73 @@
+/*
+ * @Team Group_1
+ * @LastEdited 11/4/2016 1:06AM
+ * @EditedBy Kronemeyer, Joshua
+ * @Notes 
+ * 
+ */
+
 public class Player {
 	
+	// Declare Vars
 	int playerLocation, playerCharacter, playerToken;
 	String playerName;
 	
-	public void Player() {
+	// Default Constructor
+	public Player() {
 		
+	}
+	
+	// Constructor
+	public Player(int loc, int character, int token, String name) {
+		playerLocation = loc;
+		playerCharacter = character;
+		playerToken = token;
+		playerName = name;
+	}
+	
+	
+	// Mutators
+	
+	//@param1 Player Location
+	public void setLocation(int loc) {
+		playerLocation = loc;
+	}
+	
+	//@param1 Player Character Chosen
+	public void setCharacter(int character) {
+		playerCharacter = character;
+	}
+	
+	//@param1 Unique player token
+	public void setToken(int token) {
+		playerToken = token;
+	}
+	
+	//@param1 Name set by player
+	public void setName(String name) {
+		playerName = name;
+	}
+	
+	// Accessors
+	
+	//@return Player's Location
+	public int getLocation() {
+		return playerLocation;
+	}
+	
+	//@return ID of player character
+	public int getCharacter() {
+		return playerCharacter;
+	}
+	
+	//@return Unique player token
+	public int getToken() {
+		return playerToken;
+	}
+	
+	//@return Name set by player
+	public String getName() {
+		return playerName;
 	}
 	
 }
