02038b85833ef545edf3668d4a004c0b15dfffdb
bazgrim <joshuakronemeyer@gmail.com>
2016-11-12 03:25:36 -0700

    Updated variable naming convention to match constants
    Uppercase for constants, not lowercase

--- src/EelsAndEscalatorsInterface.java
+++ src/EelsAndEscalatorsInterface.java
@@ -1,27 +1,27 @@
 
 public interface EelsAndEscalatorsInterface {
 	// Player ID's
-	int player1 = 1;
-	int player2 = 2;
-	int player3 = 3;
-	int player4 = 4;
+	int PLAYER1 = 1;
+	int PLAYER2 = 2;
+	int PLAYER3 = 3;
+	int PLAYER4 = 4;
 	
 	// Player Win Identification
-	int player1_win = 11;
-	int player2_win = 22;
-	int player3_win = 33;
-	int player4_win = 44;
+	int PLAYER1_WIN = 11;
+	int PLAYER2_WIN = 22;
+	int PLAYER3_WIN = 33;
+	int PLAYER4_WIN = 44;
 	
 	// Any string constants should be put below this line
-	String rollDiceMessage = "Please roll dice.";
-	String characterSelectMessage = "Select your character";
-	String wait_Message = "Please wait...";
-	String connecting_Message = "Please wait... Connecting...";
-	String max_Connected = "The game is full, no more players may join.";
+	String ROLLDICEMSG = "Please roll dice.";
+	String CHARSELECTMESSAGE = "Select your character";
+	String WAIT_MESSAGE = "Please wait...";
+	String CONNECTING_MESSAGE = "Please wait... Connecting...";
+	String MAX_CONNECTED = "The game is full, no more players may join.";
 	
 	// Character ID's
-	int mrKrabs = 123;
-	int squidward = 124;
-	int spongebob = 125;
-	int patrick = 126;
+	int MRKRABS = 123;
+	int SQUIDWARD = 124;
+	int SPONGEBOB = 125;
+	int PATRICK = 126;
 }
