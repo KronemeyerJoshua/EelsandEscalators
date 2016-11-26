14c9b327ff7afbff5a97aee074db95175d26f482
bazgrim <joshuakronemeyer@gmail.com>
2016-11-26 07:49:15 -0700

    Moved functions, added GUI, began request handling
    Moved functions to proper class
    Basic GUI implementation
    Incomplete action handling code

--- src/EelsAndEscalatorsServer.java
+++ src/EelsAndEscalatorsServer.java
@@ -1,30 +1,48 @@
 import java.net.*;
 import java.io.*;
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.net.*;
 import java.util.Date;
 
 public class EelsAndEscalatorsServer extends JFrame implements EelsAndEscalatorsInterface {
 	
-	// Global Vars
-	int numOfPlayers;
-	int[] turnOrder;
-	int[] playerCharacterChoice;
-	
 	public static void main(String[] args) {
 		EelsAndEscalatorsServer server = new EelsAndEscalatorsServer();
-		System.out.println("Server started");
 	}
 	
 	// Default Constructor
 	public EelsAndEscalatorsServer() {	
 		
+		JPanel contentPane;
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(860, 440, 450, 300);
+		setResizable(false);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout());
+		setContentPane(contentPane);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(new Color(18, 148, 203));
+		contentPane.add(panel, BorderLayout.SOUTH);
+        
+        //Text Area, use outputText.append() to send stuff
+        JTextArea outputText = new JTextArea();
+        JScrollPane scrollPane = new JScrollPane(outputText);
+        contentPane.add(scrollPane, BorderLayout.CENTER);
+        setVisible(true);
+        
 		// Attempt to Host Server
 		try {
 			// Harr harr, server socket is over 9000
 			ServerSocket socket = new ServerSocket(9001);
-			
+
 			while (true) {
 				
 				// Connect our players
@@ -36,6 +54,7 @@ public class EelsAndEscalatorsServer extends JFrame implements EelsAndEscalators
 				//Socket player4 = socket.accept();
 				
 				// Assign our players ID's
+				outputText.append("Player 1 now connecting from " + player1.getInetAddress().getHostAddress());
 				new DataOutputStream(player1.getOutputStream()).writeInt(PLAYER1);
 				
 				// DEBUGGING_NOTE: Players 2-4 have been disabled to facilitate testing purposes
@@ -45,39 +64,26 @@ public class EelsAndEscalatorsServer extends JFrame implements EelsAndEscalators
 				
 				// DEBUGGING NOTE: NEED TO ADD PLAYERS TO CONSTRUCTOR
 				//2-4 WHEN TESTING OF PLAYER1 IS FINISHED
-				GameSession session = new GameSession(player1);
 				
+				GameSession session = new GameSession(player1);
 				new Thread(session).start();
 			}
 		}
 		catch (Exception e) {
-			System.out.println(e.toString());
+			outputText.append(e.toString());
 		}
 	}
 	
-	// Add a player to the game
-	public void addPlayer() {
-		if (numOfPlayers < 4) {
-			numOfPlayers++;
-		}
-		else {
-			System.out.println(MAX_CONNECTED);
-		}
-	}
 	
-	// @return Returns number of players in game
-	public int getNumOfPlayers() {
-		return numOfPlayers;
-	}
-	
-	public int whosNext() {
-		
-		return 0;
-	}	
 }
 
 
-class GameSession implements Runnable, EelsAndEscalatorsInterface {
+class GameSession extends JPanel implements Runnable, EelsAndEscalatorsInterface {
+	
+	// Global Vars
+	int numOfPlayers;
+	int[] turnOrder;
+	int[] playerCharacterChoice;
 	
 	// Create our sockets to player clients
 	private Socket player1;
@@ -112,12 +118,42 @@ class GameSession implements Runnable, EelsAndEscalatorsInterface {
 			//DataOutputStream toP4 = new DataOutputStream(player4.getOutputStream());
 			
 			while (true) {
+				switch (fromP1.readInt()) {
+					case SEND_ROLL_REQUEST: // RollDice()
+											// Update player info
+											break;
+					
+				}
+					
 			}
 		}
 		catch (Exception e) {
 			System.out.println(e.toString());
 		}
 		
+	
+	
 	}
+
+	
+	// Add a player to the game
+	public void addPlayer() {
+		if (numOfPlayers < 4) {
+			numOfPlayers++;
+		}
+		else {
+			System.out.println(MAX_CONNECTED);
+		}
+	}
+	
+	// @return Returns number of players in game
+	public int getNumOfPlayers() {
+		return numOfPlayers;
+	}
+	
+	public int whosNext() {
+		
+		return 0;
+	}	
 	
 }
