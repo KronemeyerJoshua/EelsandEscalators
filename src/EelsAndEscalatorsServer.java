7d335f8d4f1b6fc9e47e43aec66a4af81e84e81d
Mangaloney <mangaloney@gmail.com>
2016-11-29 01:39:59 -0700

    Added formating, player connections, map, player input/output, GameSession constructors for players, whosNext(), rollDice, genMapDefault(),
    and movePlayer().

--- src/EelsAndEscalatorsServer.java
+++ src/EelsAndEscalatorsServer.java
@@ -8,6 +8,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.net.*;
 import java.util.Date;
+import java.util.Random;
 
 public class EelsAndEscalatorsServer extends JFrame implements EelsAndEscalatorsInterface {
 	
@@ -49,18 +50,17 @@ public class EelsAndEscalatorsServer extends JFrame implements EelsAndEscalators
 				Socket player1 = socket.accept();
 				
 				// DEBUGGING_NOTE: Players 2-4 have been disabled to facilitate testing purposes
-				//Socket player2 = socket.accept();
-				//Socket player3 = socket.accept();
-				//Socket player4 = socket.accept();
+				Socket player2 = socket.accept();
+				Socket player3 = socket.accept();
+				Socket player4 = socket.accept();
 				
 				// Assign our players ID's
-				outputText.append("Player 1 now connecting from " + player1.getInetAddress().getHostAddress());
+				outputText.append("\nPlayer 1 now connecting from " + player1.getInetAddress().getHostAddress());
 				new DataOutputStream(player1.getOutputStream()).writeInt(PLAYER1);
 				
 				// DEBUGGING_NOTE: Players 2-4 have been disabled to facilitate testing purposes
-				//new DataOutputStream(player1.getOutputStream()).writeInt(PLAYER2);
-				//new DataOutputStream(player1.getOutputStream()).writeInt(PLAYER3);
-				//new DataOutputStream(player1.getOutputStream()).writeInt(PLAYER4);
+				
+				
 				
 				// DEBUGGING NOTE: NEED TO ADD PLAYERS TO CONSTRUCTOR
 				//2-4 WHEN TESTING OF PLAYER1 IS FINISHED
@@ -81,15 +81,30 @@ public class EelsAndEscalatorsServer extends JFrame implements EelsAndEscalators
 class GameSession extends JPanel implements Runnable, EelsAndEscalatorsInterface {
 	
 	// Global Vars
-	int numOfPlayers;
+	int numOfPlayers, currentPlayerTurn, dice1, dice2, totalDice;
 	int[] turnOrder;
 	int[] playerCharacterChoice;
+	private Random rand = new Random();
+	
+	//Create the map tiles
+	final int MAP_X = 10; int MAP_Y = 3;
+	private Tile<Player>[][] map; //map
 	
 	// Create our sockets to player clients
 	private Socket player1;
-	//private Socket player2;
-	//private Socket player3;
-	//private Socket player4;
+	private Socket player2;
+	private Socket player3;
+	private Socket player4;
+	
+	//input and output steams
+	private DataInputStream fromPlayer1;
+	private DataOutputStream toPlayer1;
+	private DataInputStream fromPlayer2;
+	private DataOutputStream toPlayer2;
+	private DataInputStream fromPlayer3;
+	private DataOutputStream toPlayer3;
+	private DataInputStream fromPlayer4;
+	private DataOutputStream toPlayer4;
 	
 	// Constructor
 	// DEBUGGING NOTE: NEED TO ADD PLAYERS TO CONSTRUCTOR
@@ -99,23 +114,52 @@ class GameSession extends JPanel implements Runnable, EelsAndEscalatorsInterface
 		
 		// Initialize our player connections before game start
 		this.player1 = player1;
-		// this.player2 = player2;
-		// this.player3 = player3;
-		// this.player4 = player4;
+		
+		genMapDefault(); //generate map
+	}
+	
+	public GameSession(Socket player1, Socket player2) {
+		
+		// Initialize our player connections before game start
+		this.player1 = player1;
+		this.player2 = player2;
+		
+		genMapDefault(); //generate map
+	}
+	
+	public GameSession(Socket player1, Socket player2, Socket player3) {
+		
+		// Initialize our player connections before game start
+		this.player1 = player1;
+		this.player2 = player2;
+		this.player3 = player3;
+		
+		genMapDefault(); ////generate map
+	}
+	
+	public GameSession(Socket player1, Socket player2, Socket player3, Socket player4) {
+		
+		// Initialize our player connections before game start
+		this.player1 = player1;
+		this.player2 = player2;
+		this.player3 = player3;
+		this.player4 = player4;
+		
+		genMapDefault(); //generate map
 	}
 	
 	public void run() {
 		try {
 			// Initialize our in and out streams to send/receive from our player clients
 			DataInputStream fromP1 = new DataInputStream(player1.getInputStream());
-			//DataInputStream fromP2 = new DataInputStream(player2.getInputStream());
-			//DataInputStream fromP3 = new DataInputStream(player3.getInputStream());
-			//DataInputStream fromP4 = new DataInputStream(player4.getInputStream());
+			DataInputStream fromP2 = new DataInputStream(player2.getInputStream());
+			DataInputStream fromP3 = new DataInputStream(player3.getInputStream());
+			DataInputStream fromP4 = new DataInputStream(player4.getInputStream());
 			
 			DataOutputStream toP1 = new DataOutputStream(player1.getOutputStream());
-			//DataOutputStream toP2 = new DataOutputStream(player2.getOutputStream());
-			//DataOutputStream toP3 = new DataOutputStream(player3.getOutputStream());
-			//DataOutputStream toP4 = new DataOutputStream(player4.getOutputStream());
+			DataOutputStream toP2 = new DataOutputStream(player2.getOutputStream());
+			DataOutputStream toP3 = new DataOutputStream(player3.getOutputStream());
+			DataOutputStream toP4 = new DataOutputStream(player4.getOutputStream());
 			
 			while (true) {
 				switch (fromP1.readInt()) {
@@ -152,8 +196,63 @@ class GameSession extends JPanel implements Runnable, EelsAndEscalatorsInterface
 	}
 	
 	public int whosNext() {
-		
-		return 0;
+		if(currentPlayerTurn < 4)
+			return currentPlayerTurn+1;
+		else
+			return 0;	
 	}	
+	
+	public int setNext() {
+		if(currentPlayerTurn < 4)
+			return currentPlayerTurn++;
+		else
+			return currentPlayerTurn = 0;	
+	}	
+	
+	
+	public int rollDice(){	
+		return rand.nextInt() % 5 +1 ; // dice implementation
+	}
+	
+	public void genMapDefault(){ //regular map generation - added to server TODO
+		map = new Tile[MAP_Y][MAP_X];
+		for(int x = 0; x < MAP_X; x++){
+			for(int y = 0; y < MAP_Y; y++){
+				map[y][x] = new Tile<Player>(x, y);
+			}
+		}
+		map[0][0].setStart(true);
+		map[0][1].setEel1(true);
+		map[0][9].setEel1(true);
+		map[0][5].setEel2(true);
+		
+	}
+	
+	public void genMapRandom(){ //generates a random map - TODO
+		
+	}
+	
+	public void movePlayer(Player player){ //used after the dice has been rolled
+		map[player.getYLocation()][player.getXLocation()].removePlayer(player); //remove player from tile
+		int moveAmount;
+		
+		while(totalDice > 0){
+			if(player.getXLocation()+totalDice > MAP_X-1 || (player.getXLocation()-totalDice < 0 && player.getYLocation() == 1)){
+				moveAmount = (MAP_X-1) - player.getXLocation(); //space between the end of the map and current Tile
+				totalDice = totalDice - moveAmount - 1; //
+				player.setXLocation(MAP_X-1);
+				player.setYLocation(player.getYLocation()+1);
+			}
+			else if(player.getYLocation() == 1){ //moves player to the left if in the middle section
+				player.setXLocation(player.getXLocation()-1);
+				totalDice--;
+			}
+			else{
+				player.setXLocation(player.getXLocation()+1);
+				totalDice--;
+			}
+		}
+		map[player.getYLocation()][player.getXLocation()].addPlayer(player); //add player to tile
+	}
 	
 }
