7679cbb7fc7c63771e51fb25314a2a4613a1f28a
bazgrim <joshuakronemeyer@gmail.com>
2016-11-30 16:10:24 -0700

    MASSIVE CLEAN UP - Implementation of functional dice rolling

--- src/EelsAndEscalatorsServer.java
+++ src/EelsAndEscalatorsServer.java
@@ -19,74 +19,96 @@ public class EelsAndEscalatorsServer extends JFrame implements EelsAndEscalators
 	// Default Constructor
 	public EelsAndEscalatorsServer() {	
 		
-		JPanel contentPane;
+		// BEGIN GUI BUILDING
 		
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(860, 440, 450, 300);
-		setResizable(false);
+		JPanel		contentPane, panel;
+		JButton		exit;
+		JTextArea	outputText;
+		JScrollPane scrollPane;
+		
+		// MAIN WINDOW JPANEL
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout());
-		setContentPane(contentPane);
 		
-		JPanel panel = new JPanel();
+		// PROPERTIES OF WINDOW
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(860, 440, 450, 300);
+		setResizable(false);
+		setContentPane(contentPane);
+		setVisible(true);
+		
+		// SOUTHERN PANEL FOR GUI OBJECTS
+		panel = new JPanel();
 		panel.setBackground(new Color(18, 148, 203));
 		contentPane.add(panel, BorderLayout.SOUTH);
-        
-        //Text Area, use outputText.append() to send stuff
-        JTextArea outputText = new JTextArea();
-        JScrollPane scrollPane = new JScrollPane(outputText);
+		
+		// TERMINATE BUTTON
+        exit = new JButton("Exit");
+        panel.add(exit);
+
+        // TEXTBOX FOR DEBUGGING AND CONNECTION INFO
+        outputText = new JTextArea();
+        scrollPane = new JScrollPane(outputText);
         contentPane.add(scrollPane, BorderLayout.CENTER);
-        setVisible(true);
         
-		// Attempt to Host Server
+        // TERMINATE BUTTON ACTION LISTENER
+		exit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+		
+		// END GUI BUILDING
+        
+		// BEGIN HOSTING SERVER
 		try {
-			// Harr harr, server socket is over 9000
+			// THIS IS THE SOCKET OUR PLAYERS CONNECT ON
 			ServerSocket socket = new ServerSocket(9001);
+			
+			// DEBUG INFO OUTPUT
 			outputText.append(new Date() +
 			        ": Server started at socket 9001\n");
-			
 			outputText.append("\n" + new Date() + ": Waiting for players...\n");
 			
-			// Assign our players ID's
-			// Connect our players
+			// WAIT FOR CONNECTION OF PLAYERS, ACCEPT 4 PLAYERS THEN START
+			// DEBUGGING NOTE: ONLY 1 PLAYER IS ACTIVE TO FACILITATE TESTING
 			Socket player1 = socket.accept();
 			outputText.append("\nPlayer 1 now connecting from " + player1.getInetAddress().getHostAddress());
-			new DataOutputStream(player1.getOutputStream()).writeInt(PLAYER1);
 			
+			/*
 			Socket player2 = socket.accept();
 			outputText.append("\nPlayer 2 now connecting from " + player2.getInetAddress().getHostAddress());
-			new DataOutputStream(player2.getOutputStream()).writeInt(PLAYER2);
 			
 			Socket player3 = socket.accept();
 			outputText.append("\nPlayer 3 now connecting from " + player3.getInetAddress().getHostAddress());
-			new DataOutputStream(player3.getOutputStream()).writeInt(PLAYER3);
 			
 			Socket player4 = socket.accept();
 			outputText.append("\nPlayer 4 now connecting from " + player4.getInetAddress().getHostAddress());
-			new DataOutputStream(player4.getOutputStream()).writeInt(PLAYER4);
-
-			while (true) { //TODO - check if done
-				GameSession session = new GameSession(player1, player2, player3, player4);
-				new Thread(session).start();
-			}
+			*/
+			
+			// ALL PLAYERS HAVE BEEN CONNECTED
+			// BEGIN OUR SERVER LOGIC LOOP
+			GameSession session = new GameSession(player1);
+			new Thread(session).start();
 		}
 		catch (Exception e) {
 			outputText.append(e.toString());
 		}
+		// END HOSTING SERVER
 	}
 	
 	
 }
 
 
-class GameSession extends JPanel implements Runnable, EelsAndEscalatorsInterface {
+class GameSession implements Runnable, EelsAndEscalatorsInterface {
 	
 	// Global Vars
-	int numOfPlayers, currentPlayerTurn, dice1, dice2, totalDice, x, y;
-	int[] turnOrder;
+	int numOfPlayers, currentPlayerTurn, x, y;
 	int[] playerCharacterChoice;
-	private Random rand = new Random();
+	int[] dice;
+	Random rand = new Random();
 	
 	//Create the map tiles
 	final int MAP_X = 10; int MAP_Y = 3;
@@ -98,22 +120,13 @@ class GameSession extends JPanel implements Runnable, EelsAndEscalatorsInterface
 	private Socket player3;
 	private Socket player4;
 	
-	//input and output steams
-	private DataInputStream fromPlayer1;
-	private DataOutputStream toPlayer1;
-	private DataInputStream fromPlayer2;
-	private DataOutputStream toPlayer2;
-	private DataInputStream fromPlayer3;
-	private DataOutputStream toPlayer3;
-	private DataInputStream fromPlayer4;
-	private DataOutputStream toPlayer4;
+	// OUR CURRENT PLAYER SELECTION IN AND OUT STREAM
+	private DataInputStream currentIn;
+	private DataOutputStream currentOut;
 	
-	// Constructor
-	// DEBUGGING NOTE: NEED TO ADD PLAYERS TO CONSTRUCTOR
-	//2-4 WHEN TESTING OF PLAYER1 IS FINISHED
-	//@param1 Our socket connection to player client
+	// BEGIN CONSTRUCTORS
+	// @param1 Our socket connection to player1 client
 	public GameSession(Socket player1) {
-		
 		// Initialize our player connections before game start
 		this.player1 = player1;
 		
@@ -121,6 +134,8 @@ class GameSession extends JPanel implements Runnable, EelsAndEscalatorsInterface
 		
 	}
 	
+	// @param1 Our socket connection to player1 client
+	// @param2 Our socket connection to player2 client
 	public GameSession(Socket player1, Socket player2) {
 		
 		// Initialize our player connections before game start
@@ -131,6 +146,9 @@ class GameSession extends JPanel implements Runnable, EelsAndEscalatorsInterface
 		
 	}
 	
+	// @param1 Our socket connection to player1 client
+	// @param2 Our socket connection to player2 client
+	// @param3 Our socket connection to player3 client
 	public GameSession(Socket player1, Socket player2, Socket player3) {
 		
 		// Initialize our player connections before game start
@@ -141,6 +159,10 @@ class GameSession extends JPanel implements Runnable, EelsAndEscalatorsInterface
 		genMapDefault(); ////generate map
 	}
 	
+	// @param1 Our socket connection to player1 client
+	// @param2 Our socket connection to player2 client
+	// @param3 Our socket connection to player3 client
+	// @param4 Our socket connection to player4 client
 	public GameSession(Socket player1, Socket player2, Socket player3, Socket player4) {
 		
 		// Initialize our player connections before game start
@@ -150,122 +172,98 @@ class GameSession extends JPanel implements Runnable, EelsAndEscalatorsInterface
 		this.player4 = player4;
 		
 		genMapDefault(); //generate map
-		run();
 	}
+	
+	// END CONSTRUCTORS
+	
 	
 	public void run() {
 		try {
+			currentPlayerTurn = -1;
 			// Initialize our in and out streams to send/receive from our player clients
+			// DEBUGGING NOTE: PLAYER IN/OUT STREAMS ARE COMMENTED OUT FOR TESTING PURPOSES
 			DataInputStream fromP1 = new DataInputStream(player1.getInputStream());
+			/*
 			DataInputStream fromP2 = new DataInputStream(player2.getInputStream());
 			DataInputStream fromP3 = new DataInputStream(player3.getInputStream());
 			DataInputStream fromP4 = new DataInputStream(player4.getInputStream());
+			*/
+			
 			
 			DataOutputStream toP1 = new DataOutputStream(player1.getOutputStream());
+			/*
 			DataOutputStream toP2 = new DataOutputStream(player2.getOutputStream());
 			DataOutputStream toP3 = new DataOutputStream(player3.getOutputStream());
 			DataOutputStream toP4 = new DataOutputStream(player4.getOutputStream());
+			*/
 			
-			toP1.writeInt(SEND_ROLL_REQUEST);
-			
-			while (true) { //TODO
-				toP1.writeInt(whosNext());
-				toP2.writeInt(whosNext());
-				toP3.writeInt(whosNext());
-				toP4.writeInt(whosNext());
-				int rollRequest = fromP1.readInt();
-				
-				switch (rollRequest) { //Choice: make clientframe only send request when ready, so it can 
-					case SEND_ROLL_REQUEST: 
-						dice1 = rollDice();
-						dice2 = rollDice();
-						totalDice = dice1 + dice2;
-						toP1.writeInt(totalDice);
-						toP2.writeInt(totalDice);
-						toP3.writeInt(totalDice);
-						toP4.writeInt(totalDice);
+			toP1.writeInt(PLAYER_WAIT);
+			/*
+			toP2.writeInt(PLAYER_WAIT);
+			toP3.writeInt(PLAYER_WAIT);
+			toP4.writeInt(PLAYER_WAIT);
+			*/
+			// SERVER MAIN LOGIC LOOP
+			while (true) {
+				// DEBUGGING NOTE: ALL TURNS ARE PLAYER1 FOR TESTING PURPOSES
+				// BELOW DECIDES WHICH PLAYER IS THE CURRENT PLAYER
+				switch(PLAYER_TURN_ORDER[whosNext(currentPlayerTurn)]) {
+					case PLAYER1_TURN:
+						currentIn = fromP1;
+						currentOut = toP1;
 						break;
 						
-					default:
-						//TODO - decide necessary info for default operation
-				}
-				
-				toP1.writeInt(whosNext());
-				toP2.writeInt(whosNext());
-				toP3.writeInt(whosNext());
-				toP4.writeInt(whosNext());
-				rollRequest = fromP2.readInt();
-				
-				switch (rollRequest) { //Choice: make clientframe only send request when ready, so it can 
-					case SEND_ROLL_REQUEST: 
-						dice1 = rollDice();
-						dice2 = rollDice();
-						totalDice = dice1 + dice2;
-						toP1.writeInt(totalDice);
-						toP2.writeInt(totalDice);
-						toP3.writeInt(totalDice);
-						toP4.writeInt(totalDice);
+					case PLAYER2_TURN:
+						currentIn = fromP1;
+						currentOut = toP1;
 						break;
-					
-					default:
-						//TODO - decide necessary info for default operation
-				}
-				
-				toP1.writeInt(whosNext());
-				toP2.writeInt(whosNext());
-				toP3.writeInt(whosNext());
-				toP4.writeInt(whosNext());
-				rollRequest = fromP3.readInt();
-				
-				switch (rollRequest) { //Choice: make clientframe only send request when ready, so it can 
-					case SEND_ROLL_REQUEST: 
-						dice1 = rollDice();
-						dice2 = rollDice();
-						totalDice = dice1 + dice2;
-						toP1.writeInt(totalDice);
-						toP2.writeInt(totalDice);
-						toP3.writeInt(totalDice);
-						toP4.writeInt(totalDice);
-						break;
-				
-					default:
-						//TODO - decide necessary info for default operation
-				}
-				
-				toP1.writeInt(whosNext());
-				toP2.writeInt(whosNext());
-				toP3.writeInt(whosNext());
-				toP4.writeInt(whosNext());
-				rollRequest = fromP4.readInt();
-				
-				switch (rollRequest) { //Choice: make clientframe only send request when ready, so it can 
-					case SEND_ROLL_REQUEST: 
-						dice1 = rollDice();
-						dice2 = rollDice();
-						totalDice = dice1 + dice2;
-						toP1.writeInt(totalDice);
-						toP2.writeInt(totalDice);
-						toP3.writeInt(totalDice);
-						toP4.writeInt(totalDice);
 						
+					case PLAYER3_TURN:
+						currentIn = fromP1;
+						currentOut = toP1;
 						break;
-			
-					default:
-						//TODO - decide necessary info for default operation
-				}	
+						
+					case PLAYER4_TURN:
+						currentIn = fromP1;
+						currentOut = toP1;
+						break;
+				}
+				
+				// TELL THE CLIENT THAT IT IS THIS PLAYERS TURN
+				currentOut.writeInt(PLAYER_GO);
+				
+				// RECEIVE THE REQUEST FROM THE CLIENT
+					switch (currentIn.readInt()) {
+						case SEND_ROLL_REQUEST:
+							dice = rollDice();
+							currentOut.writeInt(dice[0]);
+							currentOut.writeInt(dice[1]);
+							currentOut.writeInt(PLAYER_WAIT);
+							break;	
+					}
+
+				}
 			}
-		}
 		catch (Exception e) {
 			System.out.println(e.toString());
 		}
-		
-	
-	
+		// END SERVER MAIN LOGIC LOOP
 	}
+	
+	
 	
 	private void sendMove(DataOutputStream out, int x, int y) throws IOException{
 		out.writeInt(x);
 		out.writeInt(y);
+	}
+	
+	
+	private int whosNext(int c) {
+		if (c > 2) 
+			c = 0;
+		else
+			c += 1;
+		return c;
 	}
 	
 	// Add a player to the game
@@ -298,8 +296,9 @@ class GameSession extends JPanel implements Runnable, EelsAndEscalatorsInterface
 	}	
 	
 	
-	public int rollDice(){	
-		return rand.nextInt() % 5 +1 ; // dice implementation
+	public int[] rollDice(){
+		int[] dice = { rand.nextInt(6) +1, rand.nextInt(6) +1 };
+		return dice; // dice implementation
 	}
 	
 	public void genMapDefault(){ //regular map generation - added to server TODO
@@ -323,6 +322,7 @@ class GameSession extends JPanel implements Runnable, EelsAndEscalatorsInterface
 	public void movePlayer(Player player, int currentXPosition, int currentYPosition){ //used after the dice has been rolled
 		map[player.getYLocation()][player.getXLocation()].removePlayer(player); //remove player from tile
 		int moveAmount;
+		int totalDice = dice[0] + dice[1];
 		
 		while(totalDice > 0){
 			if(player.getXLocation()+totalDice > MAP_X-1 || (player.getXLocation()-totalDice < 0 && player.getYLocation() == 1)){
