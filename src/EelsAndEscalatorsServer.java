b9c3b9a4330ac2375414e154131b2e1d76494589
Mangaloney <mangaloney@gmail.com>
2016-12-02 02:53:46 -0700

    Added number notifications for client, removed <player> adt, removed methods that weren't useful,
    fixed movePlayer() again, and added Tile to condense the number of java
    files.

--- src/EelsAndEscalatorsServer.java
+++ src/EelsAndEscalatorsServer.java
@@ -12,6 +12,8 @@ import java.util.Random;
 
 public class EelsAndEscalatorsServer extends JFrame implements EelsAndEscalatorsInterface {
 	
+	private static final long serialVersionUID = 1L;
+
 	public static void main(String[] args) {
 		EelsAndEscalatorsServer server = new EelsAndEscalatorsServer();
 	}
@@ -77,26 +79,37 @@ public class EelsAndEscalatorsServer extends JFrame implements EelsAndEscalators
 			// DEBUGGING NOTE: ONLY 1 PLAYER IS ACTIVE TO FACILITATE TESTING
 			Socket player1 = socket.accept();
 			outputText.append("\nPlayer 1 now connecting from " + player1.getInetAddress().getHostAddress());
+			//Notify that player is player 1
+			new DataOutputStream(player1.getOutputStream()).writeInt(PLAYER1);
 			
 			/*
 			Socket player2 = socket.accept();
 			outputText.append("\nPlayer 2 now connecting from " + player2.getInetAddress().getHostAddress());
+			//Notify that player is player 2
+			new DataOutputStream(player2.getOutputStream()).writeInt(PLAYER2);
 			
 			Socket player3 = socket.accept();
 			outputText.append("\nPlayer 3 now connecting from " + player3.getInetAddress().getHostAddress());
+			//Notify that player is player 3
+			new DataOutputStream(player3.getOutputStream()).writeInt(PLAYER3);
 			
 			Socket player4 = socket.accept();
 			outputText.append("\nPlayer 4 now connecting from " + player4.getInetAddress().getHostAddress());
+			//Notify that player is player 4
+			new DataOutputStream(player4.getOutputStream()).writeInt(PLAYER4);
 			*/
 			
 			// ALL PLAYERS HAVE BEEN CONNECTED
 			// BEGIN OUR SERVER LOGIC LOOP
-			GameSession session = new GameSession(player1);
+			outputText.append("\n" + MAX_CONNECTED);
+			GameSession session = new GameSession(player1 /*, player2, player3, player4*/); //TODO
 			new Thread(session).start();
+			session.run();
 		}
 		catch (Exception e) {
 			outputText.append(e.toString());
 		}
+		outputText.append("\n\n" + new Date() + ": Server is Closed." );
 		// END HOSTING SERVER
 	}
 	
@@ -114,7 +127,7 @@ class GameSession implements Runnable, EelsAndEscalatorsInterface {
 	
 	//Create the map tiles
 	final int MAP_X = 10; int MAP_Y = 3;
-	private Tile<Player>[][] map; //map
+	private Tile[][] map; //map
 	
 	// Create our sockets to player clients
 	private Socket player1;
@@ -128,7 +141,7 @@ class GameSession implements Runnable, EelsAndEscalatorsInterface {
 	
 	// BEGIN CONSTRUCTORS
 	// @param1 Our socket connection to player1 client
-	public GameSession(Socket player1) {
+	public GameSession(Socket player1) { 
 		// Initialize our player connections before game start
 		this.player1 = player1;
 		
@@ -158,7 +171,7 @@ class GameSession implements Runnable, EelsAndEscalatorsInterface {
 		this.player2 = player2;
 		this.player3 = player3;
 		
-		genMapDefault(); ////generate map
+		genMapDefault(); //generate map
 	}
 	
 	// @param1 Our socket connection to player1 client
@@ -174,6 +187,7 @@ class GameSession implements Runnable, EelsAndEscalatorsInterface {
 		this.player4 = player4;
 		
 		genMapDefault(); //generate map
+		
 	}
 	
 	// END CONSTRUCTORS
@@ -183,7 +197,7 @@ class GameSession implements Runnable, EelsAndEscalatorsInterface {
 		try {
 			currentPlayerTurn = -1;
 			// Initialize our in and out streams to send/receive from our player clients
-			// DEBUGGING NOTE: PLAYER IN/OUT STREAMS ARE COMMENTED OUT FOR TESTING PURPOSES
+			// DEBUGGING NOTE: TODO PLAYER IN/OUT STREAMS ARE COMMENTED OUT FOR TESTING PURPOSES
 			DataInputStream fromP1 = new DataInputStream(player1.getInputStream());
 			/*
 			DataInputStream fromP2 = new DataInputStream(player2.getInputStream());
@@ -205,41 +219,56 @@ class GameSession implements Runnable, EelsAndEscalatorsInterface {
 			toP3.writeInt(PLAYER_WAIT);
 			toP4.writeInt(PLAYER_WAIT);
 			*/
+			
 			// SERVER MAIN LOGIC LOOP
 			while (true) {
-				// DEBUGGING NOTE: ALL TURNS ARE PLAYER1 FOR TESTING PURPOSES
+				// DEBUGGING NOTE: TODO ALL TURNS ARE PLAYER1 FOR TESTING PURPOSES
 				// BELOW DECIDES WHICH PLAYER IS THE CURRENT PLAYER
 				switch(PLAYER_TURN_ORDER[whosNext(currentPlayerTurn)]) {
 					case PLAYER1_TURN:
 						currentIn = fromP1;
 						currentOut = toP1;
+						
 						break;
 						
 					case PLAYER2_TURN:
 						currentIn = fromP1;
 						currentOut = toP1;
+						
 						break;
 						
 					case PLAYER3_TURN:
 						currentIn = fromP1;
 						currentOut = toP1;
+						
 						break;
 						
 					case PLAYER4_TURN:
 						currentIn = fromP1;
 						currentOut = toP1;
+						
 						break;
 				}
 				
 				// TELL THE CLIENT THAT IT IS THIS PLAYERS TURN
+				currentOut.writeInt(whosNext(currentPlayerTurn));
 				currentOut.writeInt(PLAYER_GO);
 				
 				// RECEIVE THE REQUEST FROM THE CLIENT
 					switch (currentIn.readInt()) {
 						case SEND_ROLL_REQUEST:
 							dice = rollDice();
+							movePlayer(currentPlayerTurn);
 							currentOut.writeInt(dice[0]);
 							currentOut.writeInt(dice[1]);
+
+							// Sends to all
+							sendMove(toP1, x, y);
+							/* TODO
+							sendMove(toP2, x, y);
+							sendMove(toP3, x, y);
+							sendMove(toP4, x, y);
+							*/
 							currentOut.writeInt(PLAYER_WAIT);
 							break;	
 					}
@@ -251,8 +280,6 @@ class GameSession implements Runnable, EelsAndEscalatorsInterface {
 		}
 		// END SERVER MAIN LOGIC LOOP
 	}
-	
-	
 	
 	private void sendMove(DataOutputStream out, int x, int y) throws IOException{
 		out.writeInt(x);
@@ -282,22 +309,7 @@ class GameSession implements Runnable, EelsAndEscalatorsInterface {
 	public int getNumOfPlayers() {
 		return numOfPlayers;
 	}
-	
-	public int whosNext() {
-		if(currentPlayerTurn < 4)
-			return currentPlayerTurn+1;
-		else
-			return 0;	
-	}	
-	
-	public int setNext() {
-		if(currentPlayerTurn < 4)
-			return currentPlayerTurn++;
-		else
-			return currentPlayerTurn = 0;	
-	}	
-	
-	
+
 	public int[] rollDice(){
 		int[] dice = { rand.nextInt(6) +1, rand.nextInt(6) +1 };
 		return dice; // dice implementation
@@ -307,7 +319,7 @@ class GameSession implements Runnable, EelsAndEscalatorsInterface {
 		map = new Tile[MAP_Y][MAP_X];
 		for(int x = 0; x < MAP_X; x++){
 			for(int y = 0; y < MAP_Y; y++){
-				map[y][x] = new Tile<Player>(x, y);
+				map[y][x] = new Tile(x, y);
 			}
 		}
 		// Set Start & Finish
@@ -375,11 +387,18 @@ class GameSession implements Runnable, EelsAndEscalatorsInterface {
 	}
 	
 	//TODO - implement into server function
-	public void movePlayer(Player player){ //used after the dice has been rolled
-		int moveAmount;
+	public void movePlayer(int player){ //used after the dice has been rolled
+		int moveAmount, val;
 		int totalDice = dice[0] + dice[1];
-		x = player.getXLocation();
-		y = player.getYLocation();
+		for(int a = 0; a < MAP_X; a++){
+			for(int b = 0; b < MAP_Y; b++){				
+					val = map[b][a].getArray(player);
+					if(player == val){
+						x = a;
+						y = b;
+					}
+			}
+		}
 		
 		map[y][x].removePlayer(player); //remove player from tile
 		
@@ -394,22 +413,226 @@ class GameSession implements Runnable, EelsAndEscalatorsInterface {
 				
 			}
 			
-			else if(player.getYLocation() == 1){ //moves player to the left if in the middle section				
+			else if(y == 1){ //moves player to the left if in the middle section				
 				x--;
 				totalDice--;
 			}
 			
 			else{
-				player.setXLocation(player.getXLocation()+1);
+				x++;
 				totalDice--;
 			}
 		}
 		
 		// Transfer to other tiles if Eel or Escalator -- already transfers to other pieces
 		// Set x and y of player
-		player.setXLocation(map[y][x].getPositionX());
-		player.setYLocation(map[y][x].getPositionY());
 		map[y][x].addPlayer(player);
+		x = map[y][x].getPositionX();
+		y = map[y][x].getPositionY();
 		
 	}
+
+
+public class Tile extends JPanel {
+	
+	private static final long serialVersionUID = 1L;
+
+	//private Player[] currentPlayers; //players on tile
+	private int[] players;
+	
+	//Traits of tiles for the map
+	//eel1 = 1; 
+	//eel2 = 2; 
+	//eel3 = 3; 
+	//eel4 = 4;
+	//escalator1H = 5; 
+	//escalator2H = 6; 
+	//escalator3H = 7;
+	//escalator1T = 15; 
+	//escalator2T = 16; 
+	//escalator3T = 17;
+	//win = 8;
+	//lose = 9;
+	//start = 10; 
+	
+	private int trait = 0; //indicates type of tile
+	private int positionX = 0; //tile position
+	private int positionY = 0;	
+	
+	
+	
+	public Tile(){
+		players = new int[4];
+	}
+	
+	public Tile(int posX, int posY){
+		this();
+		positionX = posX;
+		positionY = posY;
+	}
+	
+	public void addPlayer (int player){ 
+		players[player] = player;
+	}
+	
+	public void removePlayer(int player){ 
+		players[player] = 4;
+	}
+	
+	public void resetPlayers(){
+		players = new int[4];
+	}
+	
+	//mutators
+	public void setTrait(int x){
+		trait = x;
+	}
+	
+	public void setEel1(){
+		trait = 1;
+	}
+	
+	public void setEel2(){
+		trait = 2;
+	}
+	
+	public void setEel3(){
+		trait = 3;
+	}
+	
+	public void setEel4(){
+		trait = 4;
+	}
+	
+	public void setEscalator1H(){
+		trait = 5;
+	}
+	
+	public void setEscalator2H(){
+		trait = 6;
+	}
+	
+	public void setEscalator3H(){
+		trait = 7;
+	}
+	
+	public void setEscalator1T(){
+		trait = 15;
+	}
+	
+	public void setEscalator2T(){
+		trait = 16;
+	}
+	
+	public void setEscalator3T(){
+		trait = 17;
+	}
+	
+	public void setWin(){
+		trait = 8;
+	}
+	
+	public void setLose(){
+		trait = 9;
+	}
+	
+	public void setStart(){
+		trait = 10;
+	}
+	
+	public void setPositionX(int posX){
+		positionX = posX;
+	}
+	
+	public void setPositionY(int posY){
+		positionY = posY;
+	}
+	
+	public void setPosition(int posX, int posY){
+		positionX = posX;
+		positionY = posY;
+	}
+	
+	//accessors
+	public boolean isEel1(){
+		return trait == 1;
+	}
+		
+	public boolean isEel2(){
+		return trait == 2;
+	}
+		
+	public boolean isEel3(){
+		return trait == 3;
+	}
+		
+	public boolean isEel4(){
+		return trait == 4;
+	}
+		
+	public boolean isEscalator1H(){
+		return trait == 5;
+	}
+	
+	public boolean isEscalator2H(){
+		return trait == 6;
+	}
+	
+	public boolean isEscalator3H(){
+		return trait == 7;
+	}
+	
+	public boolean isEscalator1T(){
+		return trait == 15;
+	}
+	
+	public boolean isEscalator2T(){
+		return trait == 16;
+	}
+	
+	public boolean isEscalator3T(){
+		return trait == 17;
+	}
+		
+	public boolean isWin(){
+		return trait == 8;
+	}
+	
+	public boolean isLose(){
+		return trait == 9;
+	}
+		
+	public boolean isStart(){
+		return trait == 0;
+	}
+	
+	public int getPositionX(){
+		return positionX;
+	}
+		
+	public int getPositionY(){
+		return positionY;
+	}
+	
+	public int getTrait(){
+		return trait;
+	}
+	/*
+	public Player getPlayerArray(int index){
+		return currentPlayers[index];
+	}
+	
+	public Player[] getPlayerArray(){
+		return currentPlayers;
+	}
+	*/
+	public int getArray(int index){
+		return players[index];
+	}
+	
+	public int[] getArray(){
+		return players;
+	}
+	
+}
 }
