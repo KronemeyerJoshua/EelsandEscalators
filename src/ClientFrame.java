6d16fc57df63446474c94f5f9503f440771918b7
bazgrim <joshuakronemeyer@gmail.com>
2016-12-02 20:32:20 -0700

    Don't need mouselistener for debugging anymore. Removed it.

--- src/ClientFrame.java
+++ src/ClientFrame.java
@@ -145,39 +145,6 @@ public class ClientFrame extends JFrame implements EelsAndEscalatorsInterface, R
 			}
 		});
 		
-		/*contentPane.addMouseListener(new MouseListener() {
-			public void mouseClicked(MouseEvent e) {
-				int x = e.getX();
-				int y = e.getY();
-				outputText.append("X: " + x + ", Y: " + y + "\n");
-			}
-
-			@Override
-			public void mouseEntered(MouseEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-
-			@Override
-			public void mouseExited(MouseEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-
-			@Override
-			public void mousePressed(MouseEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-
-			@Override
-			public void mouseReleased(MouseEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-
-		});*/
-		
 	}
 	
 	
