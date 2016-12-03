a5183e3dac6bccd3531d05d2986fbc6862d09a83
bazgrim <joshuakronemeyer@gmail.com>
2016-12-02 20:07:32 -0700

    Mutators for eel/esc detection

--- src/Player.java
+++ src/Player.java
@@ -1,16 +1,20 @@
 // Keeps track of player position...
 public class Player {
 	private int tilePosition;
-	private boolean lost;
+	private boolean lost, eel, esc;
 	
 	Player(int tile) {
 		tilePosition = tile;
 		lost = false;
+		esc = false;
+		eel = false;
 	}
 	
 	Player() {
 		tilePosition = 0;
 		lost = false;
+		esc = false;
+		eel = false;
 	}
 	
 	public int getPosition() {
@@ -27,5 +31,21 @@ public class Player {
 	
 	public boolean getLost() {
 		return lost;
+	}
+	
+	public boolean getEel() {
+		return eel;
+	}
+	
+	public void setEel(boolean eel) {
+		this.eel = eel;
+	}
+	
+	public void setEsc(boolean esc) {
+		this.esc = esc;
+	}
+	
+	public boolean getEsc() {
+		return esc;
 	}
 }
